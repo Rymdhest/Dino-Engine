43659a973a26a04a0beb2715a5517b40267161dc
Gustav Holmström <gustav.holmstroem@gmail.com>
2026-01-03 11:43:35 +0100

    removed packed variable name in shader

--- Dino Engine/Modelling/Procedural/ModelGenerator.cs
+++ Dino Engine/Modelling/Procedural/ModelGenerator.cs
@@ -11,6 +11,7 @@ namespace Dino_Engine.Modelling.Procedural
     {
         public static readonly glModel UNIT_SPHERE = glLoader.loadToVAO(IcoSphereGenerator.CreateIcosphere(1, new VertexMaterial(0)));
         public static readonly glModel UNIT_CONE = glLoader.loadToVAO(MeshGenerator.GenerateCone(new VertexMaterial(0)));
+        public static readonly glModel UNIT_BOX = glLoader.loadToVAO(MeshGenerator.generateBox(new VertexMaterial(0)));
 
         public static glModel GenerateHouse()
         {
